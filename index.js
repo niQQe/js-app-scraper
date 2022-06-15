ec9c751a83b07cc5f2e5970a4d3e1769f57939dd
unknown <nickewideving@gmail.com>
2022-06-15 18:44:12 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+import nodemailer from 'nodemailer';
+
+const lookingFor = 4;
+
+const getAppartments = async () => {
+	const res = await fetch('https://finfast.se/lediga-objekt');
+	const text = (await res.text()).trim();
+	const availableAppartments = text
+		.replace(/\n|\t/g, '')
+		.trim()
+		.match(/<dt class="title">(.*?)<\/dt>/g)
+		.map((val) => /<strong>(.*?)<\/strong>/g.exec(val)[0].replace(/<strong>|<\/strong>/g, ''))
+		.filter((app) => +app[0][0] === lookingFor).join`\n`;
+
+	return availableAppartments.length ? availableAppartments : null;
+};
+
+async function main() {
+	if (!(await getAppartments())) return;
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'nickewideving@gmail.com',
+			pass: 'fujwxqlgzwsqxejh',
+		},
+	});
+
+	transporter.sendMail({
+		from: 'nickewideving@gmail.com',
+		to: 'nickewideving@gmail.com',
+		subject: 'Finfast lediga 4or!!',
+		text: await getAppartments(),
+	});
+}
+
+main();
