d5ae551f8f4a55edf7f5743ef25f1200077ac590
unknown <nickewideving@gmail.com>
2022-06-18 14:18:55 +0200

    added html parser

--- index.js
+++ index.js
@@ -2,45 +2,37 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv/config';
 import nodemailer from 'nodemailer';
 import ioredis from 'ioredis';
+import { parse } from 'node-html-parser';
 
 const db = new ioredis(process.env.REDIS_URL);
-const rooms = 4;
+const lookingForSize = 4;
 const targets = {
 	Finfast: {
 		url: 'https://finfast.se/lediga-objekt',
-		scrapeFilter: (text) => {
-			const apps = text
-				.replace(/\n|\t/g, '')
-				.trim()
-				.match(/<dt class="title">(.*?)<\/dt>/g)
-				.map((val) => /<strong>(.*?)<\/strong>/g.exec(val)[0].replace(/<strong>|<\/strong>/g, ''))
-				.filter((app) => +app[0][0] === rooms).join`\n`;
-			return apps.length ? apps : '';
+		scrapeFilter: (html) => {
+			const root = parse(html);
+			const result = root.querySelectorAll('.title').reduce((acc, curr) => {
+				const app = curr.childNodes[1].childNodes[0].childNodes[0]._rawText.replace(/\n|\t/g, '');
+				const [size] = app;
+				if (lookingForSize == size) acc.push(app);
+				return acc;
+			}, []).join`\n`;
+
+			return result.length ? result : '';
 		},
 	},
 	Lundbergs: {
-		url: `https://www.lundbergsfastigheter.se/bostad/lediga-lagenheter/orebro?rum=${rooms}`,
-		scrapeFilter: (text) => {
-			const apps = text
-				.replace(/\n|\t/g, '')
-				.trim()
-				.match(/<td>(.*?)<\/td>/g)
-				.filter((row) => !row.includes('button'))
-				.map((row) => {
-					const parsedRow = row
-						.replace(/<td>|<\/td|<span>|<\/span>|\>|<\/a|<!-- --/g, '')
-						.replace(/<a/, ' %');
-					const indexOfSpecialChar = parsedRow.indexOf('%');
-					return indexOfSpecialChar != -1 ? parsedRow.slice(0, indexOfSpecialChar).trim() : parsedRow;
-				})
-				.reduce((acc, curr, index) => {
-					if (index != 0 && index % 5 === 0) curr = ',' + curr;
-					acc += ' ' + curr;
-					return acc;
-				}, '').split`,`
-				.map((t) => t.trim())
-				.filter((row) => row.includes(`Rum:${rooms}`)).join`\n`;
-			return apps.length ? apps : '';
+		url: `https://www.lundbergsfastigheter.se/bostad/lediga-lagenheter/orebro?rum=${lookingForSize}`,
+		scrapeFilter: (html) => {
+			const root = parse(html);
+			const result = root.querySelectorAll('.closed').reduce((acc, curr) => {
+				const adress = curr.childNodes[0].childNodes[0]._rawText;
+				const sizeInfo = curr.childNodes[2].childNodes[1]._rawText;
+				const [size] = sizeInfo;
+				if (lookingForSize == size) acc.push(`${adress}, ${sizeInfo}`);
+				return acc;
+			}, []).join`\n`;
+			return result.length ? result : '';
 		},
 	},
 };
@@ -58,7 +50,7 @@ const getScrapedAppartments = async () => {
 const generateMessage = (text) => {
 	let message = '';
 	for (let propertyOwner in text) {
-		if (text[propertyOwner]) message += `${propertyOwner}\n${text[propertyOwner]}\n\n`;
+		if (text[propertyOwner] > '') message += `${propertyOwner}\n${text[propertyOwner]}\n\n`;
 	}
 	return message;
 };
@@ -73,7 +65,7 @@ const sendMail = (available) => {
 	});
 	transporter.sendMail({
 		from: 'nickewideving@gmail.com',
-		to: 'nickewideving@gmail.com',
+		to: 'nickewideving@gmail.com, chilivit@hotmail.com',
 		subject: 'Lediga 4or!!',
 		text: generateMessage(available),
 	});
